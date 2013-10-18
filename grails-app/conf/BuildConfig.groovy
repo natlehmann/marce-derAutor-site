28605df990aebae4dccf107540c6d12f5b896ca3
Natalia Lehmann <natalia.lehmann@grupoesfera.com.ar>
2013-10-18 17:03:11 -0300

    se elimina configuracion duplicada de plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,24 +48,4 @@ grails.project.dependency.resolution = {
 
     pom true
 
-    plugins {
-        // plugins for the build system only
-        build ":tomcat:7.0.42"
-
-        // plugins for the compile step
-        compile ":scaffolding:2.0.0"
-        compile ':cache:1.1.1'
-	compile ":google-visualization:0.6.2"
-	compile ":csv:0.3.1"
-
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
-        runtime ":database-migration:1.3.5"
-        runtime ":jquery:1.10.2"
-        runtime ":resources:1.2"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
-    }
 }
