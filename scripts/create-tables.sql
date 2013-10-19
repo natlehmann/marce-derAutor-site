77790d39ecfb33f3c5849a0f54ad5d1743220a58
Natalia <natalia@natalia-home.(none)>
2013-10-19 00:08:35 -0300

    correccion de script

--- scripts/create-tables.sql
+++ scripts/create-tables.sql
@@ -3,7 +3,7 @@ create table usuario(
 	nombre varchar(255) not null,
 	password varchar(255) not null,
 	version bigint
-)  TYPE=innodb;
+)  ENGINE=InnoDB;
 
 CREATE UNIQUE INDEX UK_usuario_nombre ON usuario(nombre);
 
@@ -11,4 +11,4 @@ create table autor(
 	id BIGINT NOT NULL PRIMARY KEY,
 	nombre varchar(255) not null,
 	version bigint
-)  TYPE=innodb;
+)  ENGINE=InnoDB;
