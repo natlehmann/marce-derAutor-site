da05d1228932204270e43187f5391e1e3cdd4a8b
Natalia Lehmann <natlehmann@gmail.com>
2013-12-18 21:49:10 -0300

    se hace que el envio de mail se ejecute en otro thread

--- src/main/java/ar/com/marcelomingrone/derechosAutor/estadisticas/servicios/ServicioEnvioMail.java
+++ src/main/java/ar/com/marcelomingrone/derechosAutor/estadisticas/servicios/ServicioEnvioMail.java
@@ -13,6 +13,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,23 +30,40 @@ public class ServicioEnvioMail {
 	@Autowired
 	private MimeMessage mimeMessage;
 	
+	@Autowired
+	private ThreadPoolTaskExecutor taskExecutor;
+	
 	public void enviarNewsletter()
 			throws MessagingException, UnsupportedEncodingException {
 		
-		mimeMessage.setHeader("Content-Type", "application/octet-stream");
-		mimeMessage.setHeader("Content-Transfer-Encoding", "base64");
-		
-		mimeMessage.setText(new String("hola que tal".getBytes("UTF-8"), "UTF-8"), "UTF-8");
-
-		mimeMessage.setSubject("Acá va el título", "UTF-8");
-
-		mimeMessage.setSentDate(new Date());
-		mimeMessage.setRecipient(RecipientType.TO, new InternetAddress("natlehmann@yahoo.com"));
-		mimeMessage.setFrom(fromAddress);
-
-		log.debug("Previo a enviar mail.");
-		javaMailSender.send(mimeMessage);
-		log.debug("Envio exitoso.");
+		taskExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				try {
+					mimeMessage.setHeader("Content-Type", "application/octet-stream");
+					mimeMessage.setHeader("Content-Transfer-Encoding", "base64");
+					
+					mimeMessage.setText(new String("hola que tal".getBytes("UTF-8"), "UTF-8"), "UTF-8");
+					
+					mimeMessage.setSubject("Acá va el título", "UTF-8");
+					
+					mimeMessage.setSentDate(new Date());
+					mimeMessage.setRecipient(RecipientType.TO, new InternetAddress("natlehmann@yahoo.com"));
+					mimeMessage.setFrom(fromAddress);
+					
+					log.debug("Previo a enviar mail.");
+					javaMailSender.send(mimeMessage);
+					log.debug("Envio exitoso.");
+					
+				} catch (MessagingException | UnsupportedEncodingException e) {
+					// TODO !!!!
+					e.printStackTrace();
+				}
+				
+			}
+		});
 		
 	}
 
