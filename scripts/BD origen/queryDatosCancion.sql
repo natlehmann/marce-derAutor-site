ecd07f4acb9fc0f8c8c65efc02aef5675ecce282
Natalia Lehmann <natlehmann@gmail.com>
2014-06-23 18:08:05 -0300

    query para armar archivo de entrada al sistema

---
+++ scripts/BD origen/queryDatosCancion.sql
@@ -0,0 +1,54 @@
+SELECT     	Receipt.CompaniesID AS companyId,
+			Countries.CountriesID AS idPais,
+			Countries.CountryName AS nombrePais, 
+			Invoice.InvoiceDate AS fecha,
+			57 AS formatId,
+			Works.WorksID AS idCancion, 
+			Works.WorkName AS nombreCancion, 
+			CopyRight.OwnersID AS idAutor, 
+            Owners_All.UniqueName AS nombreAutor, 
+            Sources_All.SourcesID AS idFuente,
+            Sources_All.UniqueName AS nombreFuente, 
+            Rights.RightName AS rightName, 
+            CopyRight.CopyRightShare AS copyRightShares,
+            SUM(OniDetail.UnitsCollected) AS unidades,
+            SourcesExternalCodes.ExternalCode AS externalCode,
+            Receipt.CurrencyFactor AS currencyFactor, 
+            SUM(CollectionDetails.AmmountReceived) AS localCurrency
+            
+FROM        CollectionDetails INNER JOIN
+            CollectionHeaders ON CollectionDetails.CollectionHeadersID = CollectionHeaders.CollectionHeadersID INNER JOIN
+            CollectionDetailsDistinct ON CollectionDetails.CollectionDetailsDistinctID = CollectionDetailsDistinct.CollectionDetailsDistinctID INNER JOIN
+            Works ON CollectionDetailsDistinct.WorksID = Works.WorksID INNER JOIN
+            CopyRight ON Works.WorksID = CopyRight.WorksID INNER JOIN
+            Owners ON CopyRight.OwnersID = Owners.OwnersID INNER JOIN
+            Rights ON CollectionDetails.RightsID = Rights.RightsID INNER JOIN
+            OwnersSocieties ON Owners.OwnersID = OwnersSocieties.OwnersID INNER JOIN
+            Owners_All ON Owners.OwnersID = Owners_All.OwnersID INNER JOIN
+            Sources_All ON CollectionHeaders.SourcesID = Sources_All.SourcesID INNER JOIN
+            Invoice ON CollectionHeaders.InvoiceID = Invoice.InvoiceID INNER JOIN
+            ReceiptInvoice ON Invoice.InvoiceID = ReceiptInvoice.InvoiceID INNER JOIN
+            Receipt ON ReceiptInvoice.ReceiptID = Receipt.ReceiptID INNER JOIN
+            Countries ON CollectionDetails.CountriesID_Ammounts = Countries.CountriesID LEFT JOIN
+            UsageWork ON UsageWork.WorksID = Works.WorksID LEFT JOIN
+            OniDetailGroup ON OniDetailGroup.UsageWorkID = UsageWork.UsageWorkID LEFT JOIN
+            OniDetail ON OniDetail.OniDetailGroupID = OniDetailGroup.OniDetailGroupID LEFT JOIN
+			SourcesExternalCodes ON Sources_All.SourcesID = SourcesExternalCodes.SourcesID
+
+GROUP BY 	Works.WorksID,
+			Works.WorkName, 
+			CopyRight.OwnersID, 
+			CopyRight.CopyRightShare, 
+			OwnersSocieties.AuthorSocietiesID, 
+			Owners_All.UniqueName, 
+            Sources_All.UniqueName, 
+            Sources_All.SourcesID, 
+            Rights.RightName, 
+            Receipt.CurrencyFactor, 
+            Invoice.InvoiceDate, 
+            Receipt.CompaniesID, 
+            Countries.CountryName,
+            Countries.CountriesID,
+            SourcesExternalCodes.ExternalCode
+
+HAVING      (OwnersSocieties.AuthorSocietiesID = 59)
