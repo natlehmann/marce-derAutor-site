6de75f8aca834bbeca030754685421f33bd633d1
Natalia Lehmann <natlehmann@gmail.com>
2014-08-27 20:16:41 -0300

    limpieza

--- src/main/java/ar/com/marcelomingrone/derechosAutor/estadisticas/dao/CancionDao.java
+++ src/main/java/ar/com/marcelomingrone/derechosAutor/estadisticas/dao/CancionDao.java
@@ -4,21 +4,10 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import ar.com.marcelomingrone.derechosAutor.estadisticas.modelo.data.Cancion;
-
 @Repository
-public class CancionDao extends EntidadExternaDao<Cancion> {
+public class CancionDao {
 
 	@Autowired
 	private SessionFactory sessionFactoryExterno;
-	
-	public CancionDao() {
-		super(Cancion.class);
-	}
-
-	@Override
-	protected SessionFactory getSessionFactory() {
-		return sessionFactoryExterno;
-	}
 
 }
