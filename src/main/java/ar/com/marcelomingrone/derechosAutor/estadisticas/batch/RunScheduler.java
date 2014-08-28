bbb3fb9a169d0ed2fc57dfdd774d4f4f4940799d
Natalia Lehmann <natlehmann@gmail.com>
2014-08-28 13:22:05 -0300

    configuracion del scheduler

---
+++ src/main/java/ar/com/marcelomingrone/derechosAutor/estadisticas/batch/RunScheduler.java
@@ -0,0 +1,47 @@
+package ar.com.marcelomingrone.derechosAutor.estadisticas.batch;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunScheduler {
+
+	private static Log log = LogFactory.getLog(RunScheduler.class);
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	@Qualifier("importacionJob")
+	private Job importacionJob;
+
+	public void run() {
+
+		try {
+
+			String dateParam = new Date().toString();
+			JobParameters param = new JobParametersBuilder().addString("fechaEjecucion",
+					dateParam).toJobParameters();
+
+			log.info("Iniciando ejecución de importador de datos - " + dateParam);
+
+			JobExecution execution = jobLauncher.run(importacionJob, param);
+			log.info("Finaliza ejecución de importador de datos con Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+			log.error("Error en el proceso de importacion de datos", e);
+		}
+
+	}
+
+}
